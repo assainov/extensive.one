b8eaadb58abb5b5c0cd86930590074a9683f8607
Ilyas Assainov <iliyas.assainov@gmail.com>
2019-12-24 20:23:02 +0400

    Change code editor theme to New Moon

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 
 import configureStore from './src/state/store';
-// prismjs theme
-import './src/styles/prism-theme.css';
 
 // wrap the app with redux provider
 export const wrapRootElement: React.FC<any> = ({ element }) => {
