6e0b2b4b2b39d8cb84bcc454b7491becf903dd46
Ilyas Assainov <iliyas.assainov@gmail.com>
2019-12-27 18:41:28 +0400

    chore: change about author

--- content/website/config.ts
+++ content/website/config.ts
@@ -2,7 +2,7 @@ export const config = {
   siteTitle: `extensive.one`,
   siteDescription: `Personal blog of Ilyas Assainov`,
   author: `Ilyas Assainov`,
-  authorDescription: `Ilyas is a full stack developer specializing in scalable JavaScript apps. 
+  authorDescription: `Ilyas is a full stack developer specializing in extensible JavaScript solutions. 
   In this blog, he shares his daily challenges and solutions.`,
   email: `iliyas.assainov@gmail.com`,
   siteUrl: `https://www.extensive.one`,
